7bae82058c34bddcf4ef9fa87939606aabfa111d
MK-Sohan <mksohan786@gmail.com>
2022-05-06 16:51:43 +0600

    single api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
@@ -23,6 +23,13 @@ async function run() {
       const cursor = fruitscollection.find(query);
       const allfruits = await cursor.toArray();
       res.send(allfruits);
+    });
+
+    app.get("/fruit/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const singlefruit = await fruitscollection.findOne(query);
+      res.send(singlefruit);
     });
   } finally {
   }
